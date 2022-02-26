aab1e1124d7b8709e4d3ab9cfad335285db81d38
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-26 07:39:04 +0600

    added object method

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Using object literal
-const student = { name: 'faysal', job: 'developer' };
+const student2 = { name: 'faysal', job: 'developer' };
 
 // using object constructor
 const person = new Object();
@@ -7,7 +7,7 @@ console.log(person);
 
 //
 // const human = Object.create(null);
-const human = Object.create(student);
+const human = Object.create(student2);
 console.log(human.job);
 
 // from class or syntactical sugar
@@ -33,5 +33,32 @@ function Manus(name, age) {
   this.age = age;
 }
 
-const man = new Manus('golam', 12);
+const man = new Manus('rajib', 12);
 console.log(man);
+
+// different way about object method
+const student = {
+  name: 'Faysal',
+  id: 101,
+  money: 5000,
+  major: 'math',
+  isRich: false,
+  subject: ['english', 'economics', 'math 101', 'calculus'],
+  bestFriend: {
+    name: 'kundu',
+    major: 'math',
+  },
+  takeExam: function () {
+    console.log(`${this.name}, taking exam in ${this.major}`);
+  },
+  treatDey: function (expense, boksis = 50) {
+    this.money = this.money - expense - boksis;
+    return this.money;
+  },
+};
+
+student.takeExam();
+const remaining1 = student.treatDey(900, 100);
+const remaining2 = student.treatDey(500,20);
+console.log(remaining1);
+console.log(remaining2);
