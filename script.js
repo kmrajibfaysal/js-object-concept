9362d4e101c89292c997074c0eacabab4924bf4d
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-26 10:39:51 +0600

    added more object method about entries, seal , freeze , delete

--- script.js
+++ script.js
@@ -59,6 +59,37 @@ const student = {
 
 student.takeExam();
 const remaining1 = student.treatDey(900, 100);
-const remaining2 = student.treatDey(500,20);
+const remaining2 = student.treatDey(500, 20);
 console.log(remaining1);
 console.log(remaining2);
+
+// more objects
+const bottle = {
+  color: 'yellow', // {key: value} pair
+  hold: 'water',
+  price: '50',
+  isClean: true, //
+};
+// get all keys
+const keysBottle = Object.keys(bottle);
+console.log(keysBottle);
+
+// get all values
+const valueBottle = Object.values(bottle);
+console.log(valueBottle);
+
+// get both key: value in an array of array;
+const pairs = Object.entries(bottle);
+console.log(pairs);
+
+// block to delete any property of objects
+Object.seal(bottle);
+
+// remove properties from objects
+delete bottle.isClean;
+bottle.price = 100; // can change existing properties value
+bottle.height = 16; // but can't add or remove new key in an Object if the object is sealed
+console.log(bottle);
+
+// to make object completely unchangable use this below
+Object.freeze(bottle);
